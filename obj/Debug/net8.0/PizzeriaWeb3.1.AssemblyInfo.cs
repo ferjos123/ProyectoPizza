8e0039fb34de14988b518ebcf4f8bd27990087ba
Fer <asdtre175@gmail.com>
2024-11-24 01:19:39 -0500

    Cambios agregados a  la rama main 3

--- obj/Debug/net8.0/PizzeriaWeb3.1.AssemblyInfo.cs
+++ obj/Debug/net8.0/PizzeriaWeb3.1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzeriaWeb3.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b9e099e9ca2991e6bf6269f23667b68ecafd41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883533a045f7ab6ef83a3b88044071ed0495958d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzeriaWeb3.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzeriaWeb3.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
